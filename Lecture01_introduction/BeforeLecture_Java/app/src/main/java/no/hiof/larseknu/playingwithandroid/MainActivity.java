e6950e1af09302f42612d3677d5d84930f0cc883
omarsch <32501499+omarsch@users.noreply.github.com>
2020-08-18 13:46:04 +0200

    Oppgave 1 ferdig

--- Lecture01_introduction/BeforeLecture_Java/app/src/main/java/no/hiof/larseknu/playingwithandroid/MainActivity.java
+++ Lecture01_introduction/BeforeLecture_Java/app/src/main/java/no/hiof/larseknu/playingwithandroid/MainActivity.java
@@ -2,7 +2,10 @@ package no.hiof.larseknu.playingwithandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private String LOG_TAG = "PlayingWithAndroid";
@@ -15,20 +18,64 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO 1: Override the onStart() ActivityLifecycle event and add logging (Ctrl + O - Override shortcut)
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG,"onStart() kjører");
+    }
+
     //TODO 2: Override the onResume() ActivityLifecycle event and add logging
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d(LOG_TAG,"onStart() kjører");
+    }
 
     //TODO 3: Override the onPause() ActivityLifecycle event and add logging
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"onPause() kjører");
+    }
+
+
     //TODO 4: Override the onStop() ActivityLifecycle event and add logging
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"On stop kjører");
+    }
+
 
     //TODO 5: Override the onRestart() ActivityLifecycle event and add logging
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG,"OnRestart() kjører");
+    }
+
+
     //TODO 6: Override the onDestroy() ActivityLifecycle event and add logging
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy() kjører");
+    }
+
 
     //TODO 7: Create OtherActivity.java with the "New->Activity->Empty activity" dropdown menu
     //TODO 8: Add a TextView to OtherActivity with the text "Hello Android!"
 
     //TODO 11: Add a navigateToOtherActivity(View view), method for a button-click
+    public void navigateToOtherActivity(View view){
+        Intent intent= new Intent(this,OtherActivity.class);
+        startActivity(intent);
+    }
     //TODO 12: Create an Intent to start OtherActivity in the navigate... method
     //TODO 13: Start OtherActivity with the intent
 }
